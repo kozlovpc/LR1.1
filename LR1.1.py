5b9c2f9fbb0e7d31d7651579845d616bb9ba3548
Yury <kozlovpc@gmail.com>
2024-11-17 00:06:24 +0300

    Initial commit

---
+++ LR1.1.py
@@ -0,0 +1,119 @@
+# print("Задача 1")
+# print("Задание 1")
+#
+import itertools
+# first = int(input("Введите первое число: "))
+# second = int(input("Введите второе число: "))
+# third = int(input("Введите третье число: "))
+# resNegative = 0
+# for i in first, second, third:
+#     if i<0:
+#         resNegative+=1
+#
+# print("Колличество отрицательных чисел равно: ",resNegative)
+#
+# print("Задание 2")
+# first = int(input("Введите первое число: "))
+# second = int(input("Введите второе число: "))
+# third = int(input("Введите третье число: "))
+#
+# if first == third:
+#     print("Первое и третье")
+# elif first == second:
+#     print("Первое и второе")
+# elif second == third:
+#     print("Второе и третье")
+# else:
+#     print("Нет таких чисел")
+#
+# print("Задание 3")
+# first = int(input("Введите первое число: "))
+# second = int(input("Введите второе число: "))
+# third = int(input("Введите третье число: "))
+# resGeometr = (first * second* third)**(1/3)
+# print("Среднее геометрическое этих чисел равно:",round(resGeometr,2))
+
+# print("Задание 4")
+#
+# mounthYear = {"Зима":[1,2,12],
+#               "Весна":[3,4,5],
+#               "Лето":[6,7,8],
+#               "Осень":[9,10,11]}
+# key = int(input("Введите число месяца (от 1 до 12) "))
+# for i in mounthYear.values():
+#     if key in i:
+#         res = next((key for key,value in mounthYear.items() if value==i),None)
+# print(res)
+#
+# print("Задание 5")
+#
+# chetnoe = [int(input()) for i in range(4)]
+# for i in chetnoe:
+#     if i%2!=0:
+#         print(i," является нечётным числом")
+
+# print("Задание 6")
+#
+# threeVal = int(input("Введите трёхзначное число: "))
+# threeVal = list(map(int,list(str(threeVal))))
+# test = False
+# for i in threeVal:
+#     if i==0 or i==9:
+#         test = True
+# if(test):
+#     print("Есть")
+# else:
+#     print("Нет")
+
+# print("Задание 7")
+#
+# yearTest = int(input("Впишите пожалуйста год который хотите узнать: "))
+#
+# if yearTest %4 == 0 or yearTest%400==0:
+#     print("Год является високосным")
+# elif yearTest %100 == 0:
+#     print("Год не является високосным")
+# else:
+#     print("Год не является високосным")
+
+# print("Задание 8")
+#
+# testFour = list(map(int,list(input())))
+# if(len(set(testFour))==4):
+#     print("Повторяющихся чисел нет")
+# else:
+#     print("Повторяющиеся числа есть")
+
+# print("Задание 9")
+#
+# chislitel = int(input("Введите числитель "))
+# znamenatel = int(input("Введите знаменатель "))
+# if chislitel<znamenatel:
+#     print("Дробь правильная")
+# else:
+#     print("Дробь неправильная")
+
+# print("Задание 10")
+#
+# threeSum = list(map(int,list(input("Введите трёхзначное число"))))
+# if(sum(threeSum)%3==0):
+#     print("Число делиться на 3")
+# else:
+#     print("Число не делиться на 3")
+
+# print("Задание 11")
+# first = int(input("Введите первое число: "))
+# second = int(input("Введите второе число: "))
+# third = int(input("Введите третье число: "))
+# resMax = max(first, second, third)
+# print(resMax)
+
+print("Задание 12")
+
+
+
+
+
+
+
+
