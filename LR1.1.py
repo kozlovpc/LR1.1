fe577444a6833704df2a9afb32f6b39e33cdc94e
Yury <kozlovpc@gmail.com>
2024-11-18 04:37:35 +0300

    Ready 3 tasks with 4 variant

--- LR1.1.py
+++ LR1.1.py
@@ -2,6 +2,9 @@
 # print("Задание 1")
 #
 import itertools
+import random
+from os.path import isabs
+
 # first = int(input("Введите первое число: "))
 # second = int(input("Введите второе число: "))
 # third = int(input("Введите третье число: "))
@@ -107,12 +110,167 @@ import itertools
 # third = int(input("Введите третье число: "))
 # resMax = max(first, second, third)
 # print(resMax)
+#
+# print("Задание 12")
+# testTwo = list(map(int,list(input())))
+# testBool = False
+# for i in testTwo:
+#     if i==1 or i==9:
+#         testBool = True
+# if testBool:
+#     print("Есть")
+# else:
+#     print("Нет")
+# print("Задание 13")
+#
+# inside = int(input())
+#
+# if inside not in range(11,20):
+#     if inside%10==1:
+#         print(f"мы нашли {inside} гриб в лесу")
+#     elif inside%10==2 or inside%10==3 or inside%10==4:
+#         print(f"мы нашли {inside} гриба в лесу")
+#     else:
+#         print(f"мы нашли {inside} грибов в лесу")
+# else:
+#     print(f"мы нашли {inside} грибов в лесу")
+#
+# print("Задание 14")
+# inside = int(input())
+#
+# if inside not in range(11,20):
+#     if inside%10==1:
+#         print(f"Мне {inside} год")
+#     elif inside%10==2 or inside%10==3 or inside%10==4:
+#         print(f"Мне {inside} года")
+#     else:
+#         print(f"Мне {inside} лет")
+# else:
+#     print(f"Мне {inside} лет")
 
-print("Задание 12")
+# print("Задание 15")
+#
+# chetnoe = [int(input()) for i in range(4)]
+# for i in chetnoe:
+#     if i%2==0:
+#         print(i," является чётным числом")
+# print("Задание 16")
+#
+# resDict = {0:"Ноль",1:"Один",2:"Два",3:"Три",4:"Четыре",5:"Пять",6:"Шесть",7:"Семь"}
+#
+# test = int(input())
+# try:
+#     print(resDict[test])
+# except SyntaxError:
+#     print("Ошибка")
+# except KeyError:
+#     print("Ошибка")
+
+# print("Задание 2 Общее")
+#
+# print("Задание 1")
+# test = False
+# for i in range(100000,999998):
+#     if sum(list(map(int,list(str(i)))))%7==0 and sum(list(map(int,list(str((i+1))))))%7==0:
+#         test = True
+#         print(list(map(int, list(str(i)))),list(map(int, list(str((i + 1))))))
+#         print(sum(list(map(int, list(str(i))))), sum(list(map(int, list(str((i + 1)))))))
+# if(test):
+#     print("Могут")
+#
+# else:
+#     print("Не могут")
+
+# print("Задание 2")
+#
+# testSum = int(input("Укажите сумму реализации"))
+#
+# testDay = int(input("Введите заданное значение"))
+# days = 0
+# while testSum<testDay:
+#     testSum = round((((testSum/100)*3) + testSum),2)
+#     days += 1
+# print(testSum)
+# print(days)
+# print("Задание 3")
+# testCounter = int(input("Задаёте колличество учеников в классе"))
+# schoolClass = [[(random.choice(['М','Ж'])),(random.randrange(2007,2009)),(random.randrange(160,200)),(random.randrange(20,200))] for i in range(testCounter)]
+# boys = 0
+# girls = 0
+# sumBoys = 0
+# sumGirls = 0
+# testTall = 0
+# testSmallgirl = 200
+# testYearGirl = 0
+# minYearGirl = 2000
+# boyWeigth = 0
+# maxWeigth = 0
+# for i in schoolClass:
+#     print(i[0])
+#     if i[0]=="М":
+#         boys+=1
+#         sumBoys+=i[1]
+#         if testTall<i[2]:
+#             testTall=i[2]
+#             boyWeigth = i[3]
+#         if maxWeigth<i[3]:
+#             maxWeigth=i[3]
+#     elif i[0]=="Ж":
+#         girls+=1
+#         sumGirls += i[1]
+#         if testSmallgirl>i[2]:
+#             testSmallgirl=i[2]
+#             testYearGirl = i[1]
+#         if minYearGirl < i[1]:
+#             minYearGirl = i[1]
+# print(f"В классе {boys} мальчиков и {girls} девочек")
+# print(f"Средний возраст мальчиков является {2024-(sumBoys/boys)}, а средний возраст девочек является {2024 -(sumGirls/girls)}")
+# if maxWeigth==boyWeigth:
+#     print(f"Самый высокий рост мальчика {testTall} но при этом его вес {boyWeigth} кг является макисмальным среди мальчиков")
+# else:
+#     print(f"Максимальный вес среди мальчиков является {maxWeigth} при условии что вес самого высокого является {boyWeigth}")
+#
+# if testYearGirl==minYearGirl:
+#     print(f"Рост самой низкой девочки является {testSmallgirl} и она будет самой юной среди девочек с возрастом {2024-testYearGirl}")
+# else:
+#     print(f"Минимальный возраст является {2024-minYearGirl} при этом возраст самой маленькой девочки {2024-testYearGirl}")
+#
+#
+# print("Задание 4")
+#
+# resultSum = 0
+#
+# yearBuy = int(input("Введите пожалуйста в течении скольких лет закупалось оборудование: "))
+# countOfEquipment = int(input("Введите пожалуйста колличество оборудования: "))
+# percentOfObsolescence = int(input("Введите пожалуйста процент устаревания оборудования: "))
+#
+# testEquipment = [int(input(f"Введите пожалуйста цену оборудования номер {i+1}: ")) for i in range(countOfEquipment)]
+# readyEquipment = [0 for i in range(countOfEquipment)]
+# prebuildSum = [0 for i in range(countOfEquipment)]
+# resultSum = [0 for i in range(countOfEquipment)]
+# for y in range(yearBuy-1):
+#     for j in range(yearBuy - y):
+#         for q in range(len(readyEquipment)):
+#             test = round(float(percentOfObsolescence)/float(100),1)
+#             test = (1 - test)**(j)
+#             readyEquipment[q] = round(testEquipment[q]*test,1)
+#     resultSum = [x+y for x,y in zip(resultSum, readyEquipment)]
+# for i in range(len(resultSum)):
+#     resultSum[i] += testEquipment[i]
+#
+# for i in range(len(readyEquipment)):
+#     print(f"Цена всего закупленного оборудования номер {i+1} будет равна {round(resultSum[i],2)}")
+#
+# print("Остальные доделать")
 
 
+print("Задача 3")
 
+print("Задание 4")
 
+testCount = int(input("Введите колличество чисел: "))
+a = [random.randrange(0,99) for i in range(testCount)]
+print(f"Минимальное число равно {min(a)}")
 
 
 
